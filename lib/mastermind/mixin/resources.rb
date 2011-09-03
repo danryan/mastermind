c62f3fde48d4de00bb770e1e94b50f7c1169c91e
Dan Ryan <dan@appliedawesome.com>
2011-09-02 23:27:00 -0400

    remove unneeded comments

--- lib/mastermind/mixin/resources.rb
+++ lib/mastermind/mixin/resources.rb
@@ -42,7 +42,6 @@ module Mastermind
           new_resource.action ( new_resource.action || new_resource.default_action)
           new_resource.instance_eval(&block)
           tasks << new_resource
-          # execute(new_resource, new_resource.action)
         end
       end
       
