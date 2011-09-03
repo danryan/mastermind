75899d23be58c13e6381926a7b73a27e5cb70147
Dan Ryan <dan@appliedawesome.com>
2011-09-02 23:26:15 -0400

    complete ec2_server provider

--- lib/mastermind/provider/server/ec2.rb
+++ lib/mastermind/provider/server/ec2.rb
@@ -18,12 +18,15 @@ module Mastermind
         
         def create          
           requires :image_id, :flavor_id, :key_name, :availability_zone, :groups
+          
+          Mastermind::Log.debug
           server = connection.servers.create(
             :image_id => new_resource.image_id,
             :flavor_id => new_resource.flavor_id,
             :groups => new_resource.groups,
             :key_name => new_resource.key_name,
-            :availability_zone => new_resource.availability_zone
+            :availability_zone => new_resource.availability_zone,
+            :tags => new_resource.tags
           )
           server.wait_for { ready? }
           new_resource.update(server.attributes)
@@ -33,15 +36,48 @@ module Mastermind
         def destroy
           requires :instance_id
           
-          puts "Destroy!"
+          server = connection.servers.get(new_resource.instance_id)
+          server.destroy
+          Mastermind::Log.info "Destroying EC2 server #{new_resource.instance_id}"
+          server.wait_for { state == 'terminated' }
+          new_resource.update(server.attributes)
+          return new_resource
         end
 
         def stop
-          puts "Stop!"
+          requires :instance_id
+          
+          server = connection.servers.get(new_resource.instance_id)
+          server.stop
+          Mastermind::Log.info "Stopping EC2 server #{new_resource.instance_id}"
+          server.wait_for { state == 'stopped' }
+          new_resource.update(server.attributes)
+          return new_resource        
         end
 
         def start
-          puts "Start!"
+          requires :instance_id
+          
+          server = connection.servers.get(new_resource.instance_id)
+          server.start
+          Mastermind::Log.info "Starting EC2 server #{new_resource.instance_id}"
+          server.wait_for { state == 'running' }
+          new_resource.update(server.attributes)
+          return new_resource          
+        end
+        
+        def restart
+          requires :instance_id
+          
+          server = connection.servers.get(new_resource.instance_id)
+          server.stop
+          Mastermind::Log.info "Stopping EC2 server #{new_resource.instance_id}"
+          server.wait_for { state == 'stopped' }
+          server.start
+          Mastermind::Log.info "Starting EC2 server #{new_resource.instance_id}"
+          server.wait_for { state == 'running' }
+          new_resource.update(server.attributes)
+          return new_resource
         end
         
       end
