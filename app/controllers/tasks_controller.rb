9305a17571b7d591711d564468f5630636f9f283
Dan Ryan <dan@appliedawesome.com>
2011-11-12 14:24:48 -0500

    bumpo

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+
+  def execute
+  end
+end
