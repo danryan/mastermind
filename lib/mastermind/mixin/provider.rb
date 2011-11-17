f129eb059a7aa2a4e1cb55148404fe5fb4d7b444
Dan Ryan <dan@appliedawesome.com>
2011-11-17 03:21:54 -0500

    put mixin into lib

---
+++ lib/mastermind/mixin/provider.rb
@@ -0,0 +1,81 @@
+module Mastermind
+  module Mixin
+    module Provider
+      extend ActiveSupport::Concern
+      include Ascribe::Attributes
+
+      included do
+        extend ActiveSupport::DescendantsTracker
+      end
+
+      module ClassMethods
+        def provider_name(provider_name=nil)
+          @provider_name = provider_name.to_s if !provider_name.nil?
+          Mastermind::Registry.providers[@provider_name] = self
+          attribute :provider_name, [String, Symbol], :default => @provider_name, :required => true
+
+          return @provider_name
+        end
+
+        def actions(*args)
+          @actions = args.map(&:to_s) if !args.empty?
+          attribute :actions, Array, :default => @actions
+          return @actions
+        end
+
+        def action(name, &block)
+          define_method name do |&block|
+            instance_eval(block)
+          end
+        end
+
+        def find_by_name(name)
+          Mastermind::Registry.providers[name.to_s]
+        end
+
+        def from_hash(hash)
+          provider = find_by_name(hash["provider_name"])
+          result = provider.new(hash)
+          return result
+        end
+
+        def from_json(json)
+          hash = Yajl.load(json)
+          provider = find_by_name(hash["provider_name"])
+          result = provider.new(hash)
+          return result
+        end
+
+      end
+
+      module InstanceMethods
+
+        def requires(*args)
+          missing = []
+          args.each do |arg|
+            unless new_resource.send("#{arg}") || new_resource.options.has_key?(arg)
+              missing << arg
+            end
+          end
+          if missing.length == 1
+            raise(ArgumentError, "#{missing.first} is required for this operation")
+          elsif missing.any?
+            raise(ArgumentError, "#{missing[0...-1].join(", ")} and #{missing[-1]} are required for this operation")
+          end
+
+        end
+
+        def to_hash
+          result = attributes.merge(options)
+          return result
+        end
+
+        def to_json(*a)
+          result = Yajl.dump(to_hash, *a)
+          return result
+        end
+      end
+
+    end
+  end
+end
---
+++ lib/mastermind/mixin/resource.rb
@@ -0,0 +1,102 @@
+module Mastermind
+  module Mixin
+    module Resource
+      extend ActiveSupport::Concern
+      include Ascribe::Attributes
+
+      included do
+        extend ActiveSupport::DescendantsTracker
+      end
+
+      module ClassMethods
+        def resource_name(resource_name=nil)
+          @resource_name = resource_name.to_s if !resource_name.nil?
+          Mastermind::Registry.resources[@resource_name] = self
+          attribute :resource_name, [String, Symbol], :default => @resource_name
+          return @resource_name
+        end
+
+        def provider_name(provider_name=nil)
+          @provider_name = provider_name.to_s if !provider_name.nil?
+          attribute :provider_name, [String, Symbol], :default => @provider_name
+          return @provider_name
+        end
+
+        def provider 
+          attribute :provider, Object
+          return @provider
+        end
+
+        def default_action(name=nil)
+          @default_action = name.to_s if !name.nil?
+          attribute :default_action, [String, Symbol], :default => @default_action
+          return @default_action
+        end
+
+        def find_by_name(name)
+          Mastermind::Registry.resources[name]
+        end
+
+        def dsl_method(name, resource, &block)
+          new_resource = resource.new
+          new_resource.name name
+          new_resource.action ( new_resource.action || new_resource.default_action)
+          new_resource.instance_eval(&block)
+          tasks << new_resource
+        end
+
+        def from_hash(hash)
+          resource = find_by_name(hash["resource_name"])
+          result = resource.new(hash)
+          return result
+        end
+
+        def from_json(json)
+          hash = Yajl.load(json)
+          resource = find_by_name(hash["resource_name"])
+          result = resource.new(hash)
+          return result
+        end
+      end
+
+      module InstanceMethods
+
+        def initialize(attrs={})
+          @provider = Mastermind::Registry.providers[self.class.provider_name]
+          super(attrs)
+        end
+
+        def execute_only_if(only_if)
+          res = instance_eval { only_if.call }
+          unless res
+            return false
+          end
+          true
+        end
+
+        def execute_not_if(not_if)
+          res = instance_eval { not_if.call }
+          if res
+            return false
+          end
+          true
+        end
+
+        def to_hash
+          result = attributes.merge(options)
+          return result
+        end
+
+        def to_json(*a)
+          result = Yajl.dump(to_hash, *a)
+          return result
+        end
+
+        def to_s
+          "resource[#{resource_name}::#{name}]"
+        end
+
+      end  
+    end
+  end
+end
